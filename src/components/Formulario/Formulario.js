9b833df3d4b86c6ec22a4a573e90a38b020321c4
NICOLAS GABRIEL SIMONE <47805665@ort>
2024-05-08 09:02:50 -0300

    A

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import './Formulario.css';
 export const Formulario = () => {
+ const [citas, SetCitas] = useState();
     
 }
+
 export default Formulario;
--- src/components/Listado/Listado.js
+++ src/components/Listado/Listado.js
@@ -1,3 +1,5 @@
+import React,{useState} from "react";
+import './Listado.css';
 export const Listado = () => {
     
 }
